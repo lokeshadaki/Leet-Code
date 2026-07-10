58f2139880aac6bdb82bc631868d904c1be126c5
Lokesh Adaki <lokeshadaki07@gmail.com>
2026-07-10 15:14:12 +0530

    Rename class to LengthOfLongestSubString

---
+++ LengthOfLongestSubString.java
@@ -0,0 +1,24 @@
+class LengthOfLongestSubString {
+    public int lengthOfLongestSubstring(String s) {
+        int[] lastIndex = new int[128]; // ASCII character set
+        Arrays.fill(lastIndex, -1);
+        
+        int maxLength = 0;
+        int windowStart = 0;
+        
+        for (int windowEnd = 0; windowEnd < s.length(); windowEnd++) {
+            char currentChar = s.charAt(windowEnd);
+            
+            // If character was seen before AND is within current window,
+            // move windowStart past its last occurrence
+            if (lastIndex[currentChar] >= windowStart) {
+                windowStart = lastIndex[currentChar] + 1;
+            }
+            
+            lastIndex[currentChar] = windowEnd;
+            maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+        }
+        
+        return maxLength;
+    }
+}
