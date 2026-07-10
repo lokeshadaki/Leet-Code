6a4e37bfdbf8192682611aa405d43c9ac3e08b59
Lokesh Adaki <lokeshadaki07@gmail.com>
2026-07-10 15:09:56 +0530

    Add TwoSum class with twoSum method
    Implement TwoSum class with twoSum method to find indices of two numbers that add up to a target.

---
+++ TwoSum.java
@@ -0,0 +1,19 @@
+class TwoSum{
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+
+    public static void main(String args[]){
+        Solution s=new Solution();
+        int num[]={2,7,11,15};
+        int tar=5;
+        int sums[]=s.twoSum(num,tar);
+    }
+}
