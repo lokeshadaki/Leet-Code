bf9e1b92065c3272210f1a54d5a0b0df31c3265d
Lokesh Adaki <lokeshadaki07@gmail.com>
2026-07-15 21:36:58 +0530

    Implement longest common prefix algorithm

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,19 @@
+class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        String first = strs[0];
+        int i,j;
+        
+        for(i=0 ; i < first.length() ; i++ )
+        {
+            char ch[] = first.toCharArray();
+            for( j=1 ; j < strs.length ; j++)
+            {
+                if(i >= strs[j].length() || strs[j].charAt(i) != ch[i])
+                {
+                    return first.substring(0,i);
+                }
+            }
+        }
+        return first;
+    }
+}
