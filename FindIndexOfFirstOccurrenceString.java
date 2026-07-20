e5588e3b94135095ef408aee5124d7c41fc53bf1
Lokesh Adaki <lokeshadaki07@gmail.com>
2026-07-20 20:33:05 +0530

    Rename class to FindIndexOfFirstOccurrenceString

---
+++ FindIndexOfFirstOccurrenceString.java
@@ -0,0 +1,20 @@
+class FindIndexOfFirstOccurrenceString {
+    public int strStr(String haystack, String needle) {
+        
+        for(int i=0 ;i <=(haystack.length() - needle.length()); i++)
+        {
+            boolean match=true;
+            for(int j=0 ;j< needle.length() ; j++)
+            {
+                if(haystack.charAt(i+j)!=needle.charAt(j))
+                {
+                    match=false;
+                    break;
+                }
+            }
+            if(match)
+                return i;
+        }
+        return -1;
+    }
+}
