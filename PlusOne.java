e6019313853ca77d02959386cbd50cda30653970
Lokesh Adaki <lokeshadaki07@gmail.com>
2026-08-01 07:15:36 +0530

    Implement plusOne method in PlusOne class

---
+++ PlusOne.java
@@ -0,0 +1,20 @@
+class PlusOne {
+    public int[] plusOne(int[] digits) {
+        for(int i=digits.length-1; i>=0;i--)
+        {
+            if(digits[i]==9)
+            {
+                digits[i]=0;
+            }
+            else
+            {
+                digits[i]++;
+                return digits;
+            }
+        }
+
+        int digit[]=new int[digits.length +1];
+        digit[0]=1;
+        return digit;
+    }
+}
